1a1f599b9a4377c13f80fc1b77016ab4e2128046
Nick Beeuwsaert <nick.beeuwsaert@gmail.com>
2015-01-29 22:03:46 -0600

    Fixed choices

--- install.py
+++ install.py
@@ -8,11 +8,22 @@ ROOT_DIR=os.path.dirname(__file__)
 DOTFILES_DIR=os.path.join(ROOT_DIR, "dotfiles")
 HOME = os.path.expanduser('~')
 
-def prompt(text, default):
+def prompt(text, default = None):
     import sys
     sys.stdout.write(text)
     sys.stdout.flush()
     return sys.stdin.readline().rstrip('\n') or default
+
+def launch_diff(filea, fileb):
+    subprocess.call(['vimdiff', ourfile, dotfile])
+
+def choice(p, choices):
+    while True:
+        choice = prompt(p);
+        if choice not in choices:
+            print("Try again.")
+            continue
+        if choices[choice](): break
 
 for dirpath, dirnames, filenames in os.walk(DOTFILES_DIR):
     for filename in filenames:
@@ -25,28 +36,20 @@ for dirpath, dirnames, filenames in os.walk(DOTFILES_DIR):
         if not os.path.exists(dotfile_path):
             print("Creating \"{}\"...".format(dotfile_path))
             os.makedirs(dotfile_path)
-        #Otherwise see if the file alreadt exists
+        #Otherwise see if the file already exists
         elif os.path.lexists(dotfile):
             if os.path.realpath(ourfile) == os.path.realpath(dotfile):
                 print("Skipping {}, already symlinked...".format(dotfile)) 
             else:
                 print("Uh oh! {} already exists! What would you like to do?".format(dotfile))
-                # This choice stuff is ugly, needs a rewrite when I get time
-                # Also, for rigt now replacing the file isn't an option
+                # for right now, simply replacing the file isn't an option
                 # Because I want it to be difficult for me to remove a important file
-                choices = ["I", "D"]
-                while True:
-                    choice = prompt("[I]gnore, View [D]iff: ", "I").upper()
-                    if choice not in choices:
-                        print("Invalid selection")
-                    else:
-                        break
-                if choice == "D":
-                    print("Lauching vimdiff...")
-                    # TODO: Go back up to choices after this
-                    subprocess.call(['vimdiff', ourfile, dotfile])
-                elif choice == "I":
-                    print("Ignoring collision...")
+                # And I could probably merge over the relevant lines in vimdiff anyway
+                choice("[I]gnore it, View the [D]iff: ", {
+                    'I': lambda: True,
+                    'D': lambda: launch_diff(ourfile, dotfile)
+                })
+        # File doesn't exist, symlink it!
         else:
             print("symlinking \"{}\" to \"{}\"".format(os.path.abspath(ourfile), dotfile))
             os.symlink(os.path.abspath(ourfile), dotfile)
